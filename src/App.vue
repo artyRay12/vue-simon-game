496f5915a1d8be6e07f1af55d1a8cae9c7f7f09d
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-08-08 18:32:02 +0300

    add sounds and styles

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,58 @@
 <template>
-    <div id="app" class="game-wrapper">
-        <div class="buttons-wrapper">
-            <div class="button red" :class="{'active':buttonToAnimate==1}" @click="handleClick(1)"></div>
-            <div
-                class="button green"
-                :class="{'active':buttonToAnimate==2}"
-                @click="handleClick(2)"
-            ></div>
-            <div class="button blue" :class="{'active':buttonToAnimate==3}" @click="handleClick(3)"></div>
-            <div
-                class="button yellow"
-                :class="{'active':buttonToAnimate==4}"
-                @click="handleClick(4)"
-            ></div>
-            {{series}}
+    <div>
+        <div id="app" class="game-wrapper" :class="{'blur': isLose}">
+            <div class="buttons-wrapper">
+                <div
+                    class="button red"
+                    :class="{'active':buttonToAnimate==1}"
+                    @click="handleClick(1)"
+                ></div>
+                <div
+                    class="button green"
+                    :class="{'active':buttonToAnimate==2}"
+                    @click="handleClick(2)"
+                ></div>
+                <div
+                    class="button blue"
+                    :class="{'active':buttonToAnimate==3}"
+                    @click="handleClick(3)"
+                ></div>
+                <div
+                    class="button yellow"
+                    :class="{'active':buttonToAnimate==4}"
+                    @click="handleClick(4)"
+                ></div>
+            </div>
+            <div class="game-options">
+                <div class="difficult-button-wrapper">
+                    <input
+                        type="radio"
+                        name="difficultLevel"
+                        value="1500"
+                        @click="difficultLevelClickHandler(1500)"
+                        checked
+                    /> easy
+                    <input
+                        type="radio"
+                        name="difficultLevel"
+                        value="1000"
+                        @click="difficultLevelClickHandler(1000)"
+                    /> normal
+                    <input
+                        type="radio"
+                        name="difficultLevel"
+                        value="450"
+                        @click="difficultLevelClickHandler(450)"
+                    /> hard
+                </div>
+                <p>Раунд: {{series.length}}</p>
+                <button @click.prevent="restart" class="start-game-btn">Start</button>
+            </div>
         </div>
-        <div class="diffcult-button-wrapper">
-            <input
-                type="radio"
-                name="difficultLevel"
-                value="1500"
-                @click="difficultLevelClickHandler(1500)"
-            /> easy
-            <input
-                type="radio"
-                name="difficultLevel"
-                value="1000"
-                @click="difficultLevelClickHandler(100)"
-            /> normal
-            <input
-                type="radio"
-                name="difficultLevel"
-                value="450"
-                @click="difficultLevelClickHandler(450)"
-            /> hard
+        <div class="lose-alert" v-if="this.isLose">
+            <p class="lose-alert-text">You lose :(</p>
+            <button class="lose-alert-btn" @click.prevent="restart">ok</button>
         </div>
-        <button @click="startGame()">Start</button>
     </div>
 </template>
 <script>
@@ -44,148 +61,96 @@
 export default {
     data() {
         return {
-            series: new Array(50),
-            buttonDisabled: false,
-            currentRound: 0,
-            buttonToAnimate: 0,
-            difficult: 1,
+            buttonDisabled: true,
+            difficult: 1500,
+            series: [],
             clickCounter: 0,
+            buttonToAnimate: 0,
+            isLose: false,
+
+            sounds: {
+                buttonSounds: [
+                    "https://shorturl.at/bnvD8",
+                    "https://shorturl.at/nFKVZ",
+                    "https://shorturl.at/blGX6",
+                    "https://shorturl.at/aswS9",
+                ],
+                error: "https://shorturl.at/fgU17",
+            },
         };
     },
 
     methods: {
-        startGame(difficultLevel = 1500) {
-          console.log(difficultLevel)
-            //this.seriesInit();
-            this.series = [
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-            ];
-            this.difficult = difficultLevel;
-            this.currentRound = 7;
-            this.clickCounter = 0;
-            this.animateSeries(this.series.slice(0, this.currentRound));
+        restart() {
+            this.isLose = false;
+            this.series = [];
+            this.newRound();
         },
 
-        seriesInit() {
-            this.series = new Array(50)
-                .fill("")
-                .map((elem) => Math.ceil(Math.random() * 4));
-        },
-
-        lightUp(button) {
-            this.buttonToAnimate = button;
-        },
-
-        difficultLevelClickHandler(level) {
-            this.startGame(level)
-        },
-
-        handleClick(button) {
-            if (!this.buttonDisabled) {
-                if (button !== this.series[this.clickCounter]) {
-                    alert("looser");
-                }
-
-                if (this.clickCounter + 1 === this.currentRound) {
-                    alert("alert new round");
-                    this.clickCounter = 0;
-                    this.currentRound++;
-                    this.animateSeries(this.series.slice(0, this.currentRound));
-                } else {
-                    this.clickCounter++;
-                }
-            }
+        newRound() {
+            setTimeout(() => {
+                this.clickCounter = 0;
+                this.series.push(Math.ceil(Math.random() * 4));
+                this.animateSeries(this.series);
+            }, 1000);
         },
 
         animateSeries(seriesToAnimate) {
             this.buttonDisabled = true;
             let timer = 0;
+
             seriesToAnimate.forEach((elem, ind) => {
-                setTimeout(() => {
-                    this.lightUp(elem);
-
-                    if (ind === seriesToAnimate.length - 1) {
-                        this.buttonDisabled = false;
-                        this.lightUp(0);
-                    }
-                }, timer);
+                this.lightUpWithTimer(elem, timer, timer + this.difficult / 2);
 
                 setTimeout(() => {
-                    this.lightUp(0);
-                }, timer + this.difficult / 2);
+                    this.buttonDisabled = false;
+                }, this.difficult * this.series.length - 100);
 
                 timer += this.difficult;
             });
         },
+
+        lightUpWithTimer(button, timeout, duration) {
+            setTimeout(() => {
+                this.buttonToAnimate = button;
+                this.playSound(this.sounds.buttonSounds[button - 1]);
+            }, timeout);
+
+            setTimeout(() => {
+                this.buttonToAnimate = 0;
+            }, duration);
+        },
+
+        difficultLevelClickHandler(level) {
+            this.difficult = level;
+        },
+
+        playSound(button) {
+            let audio = new Audio(button);
+            audio.play();
+        },
+
+        handleClick(button) {
+            if (!this.buttonDisabled) {
+                if (button !== this.series[this.clickCounter]) {
+                    this.isLose = true;
+                    return;
+                }
+
+                this.lightUpWithTimer(button, 0, 500);
+
+                if (this.clickCounter + 1 === this.series.length) {
+                    this.newRound();
+                } else {
+                    this.clickCounter++;
+                }
+            } else {
+                this.playSound(this.sounds.error);
+            }
+        },
     },
 };
 </script>
-<style>
-.game-wrapper {
-    max-width: 100%;
-    width: 900px;
-    margin: 0 auto;
-    background-color: darkslategray;
-    padding: 50px;
-    box-sizing: border-box;
-    display: flex;
-}
 
-.button {
-    max-width: 80px;
-    height: 80px;
-    width: 100%;
-    opacity: 0.6;
-    transition: 0.2s;
-    border-radius: 4px;
-}
-
-.button:hover,
-.active {
-    opacity: 1;
-    cursor: pointer;
-}
-.red {
-    background-color: red;
-}
-
-.blue {
-    background-color: blue;
-}
-
-.green {
-    background-color: green;
-}
-
-.yellow {
-    background-color: yellow;
-}
-
-.buttons-wrapper {
-    max-width: 200px;
-    widows: 100%;
-    display: flex;
-    flex-wrap: wrap;
-}
+<style src="./App.scss" lang="scss">
 </style>
